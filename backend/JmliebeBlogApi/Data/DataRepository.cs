99fc0d388dc177feceb6094298a0e920111863e4
Jordan M. Liebe <Jordan.Liebe@outlook.com>
2020-07-18 19:58:39 -0700

    Added Controllers and primative Interface

---
+++ backend/JmliebeBlogApi/Data/DataRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JmliebeBlogApi.Data
+{
+    public class DataRepository : IDataRepository
+    {
+        public int CreateNewPost(int Data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ backend/JmliebeBlogApi/Models/SinglePost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JmliebeBlogApi.Models
+{
+    public class SinglePost
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+        public string Author { get; set; }
+    }
+}
